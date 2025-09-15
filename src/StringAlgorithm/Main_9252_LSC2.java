c741c081fbbb284f3c782cac44f025a6f9820c6f
returns0825 <junsu120202@gmail.com>
2025-09-15 13:26:51 +0900

    0915 ps

---
+++ src/StringAlgorithm/Main_9252_LSC2.java
@@ -0,0 +1,45 @@
+package StringAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_9252_LSC2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int n = str1.length();
+        int m = str2.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = n, j = m;
+        while(i > 0 && j > 0) {
+            if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                sb.append(str1.charAt(i-1));
+                i--; j--;
+            } else {
+                if(dp[i-1][j] > dp[i][j-1]) i--;
+                else j--;
+            }
+        }
+
+        System.out.println(dp[n][m]);
+        if(dp[n][m] > 0) {
+            System.out.println(sb.reverse().toString());
+        }
+    }
+}
