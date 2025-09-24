fed614888f0b22f6ae8f7bbbb8750b887c20343b
returns0825 <junsu120202@gmail.com>
2025-09-24 09:57:28 +0900

    0924 ps

---
+++ src/Implementation/Main_24263_알고리즘수업알고리즘의수행시간2.java
@@ -0,0 +1,13 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_24263_알고리즘수업알고리즘의수행시간2 {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+        System.out.println(1);
+	}
+}
