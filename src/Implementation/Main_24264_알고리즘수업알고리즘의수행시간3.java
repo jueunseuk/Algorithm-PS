83480ebbfc0576952882dcb6b37d8502f802f024
returns0825 <junsu120202@gmail.com>
2025-09-25 17:10:35 +0900

    0925 ps

---
+++ src/Implementation/Main_24264_알고리즘수업알고리즘의수행시간3.java
@@ -0,0 +1,14 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_24264_알고리즘수업알고리즘의수행시간3 {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long in = Long.parseLong(br.readLine());
+        System.out.println(in*in);
+        System.out.println(2);
+	}
+}
