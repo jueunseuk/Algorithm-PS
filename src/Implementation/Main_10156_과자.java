cb816745e5ec17608a151aae8e7126d6512802f8
returns0825 <junsu120202@gmail.com>
2025-10-20 08:58:48 +0900

    1020 ps

---
+++ src/Implementation/Main_10156_과자.java
@@ -0,0 +1,20 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_10156_과자 {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        System.out.println(a*b-c >= 0 ? a*b-c : 0);
+	}
+}
---
+++ src/Implementation/Main_2914_저작권.java
@@ -0,0 +1,19 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2914_저작권 {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        System.out.println(a*(b-1)+1);
+	}
+}
