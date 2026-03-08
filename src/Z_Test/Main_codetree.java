5ccbb5ea300ba4ec521bf4102e0da925b8d06fc2
returns0825 <junsu120202@gmail.com>
2026-03-08 14:53:01 +0900

    0308 ps

---
+++ src/Z_Test/Main_codetree.java
@@ -0,0 +1,12 @@
+package Z_Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_codetree {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.printf("%.2f", Double.parseDouble(br.readLine()));
+	}
+}
